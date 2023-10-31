c279cc800253175d325b08e2fe2bb765a47c3b8f
Bartosz Ryfa <132827271+bartoszryfa@users.noreply.github.com>
2023-10-31 23:38:13 +0100

    add date exercise

---
+++ Date/StringToDate/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import javax.management.MBeanException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeParseException;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        LocalDate dateNow = LocalDate.now();
+
+        System.out.println("When is your birthday? (####-01-31)");
+        String dateString = reader.readLine();
+
+        try {
+            LocalDate dateOfBirth = LocalDate.parse(dateString);
+            Period betweenBirthday = Period.between(dateOfBirth, dateNow);
+            int years = betweenBirthday.getYears();
+            int months = betweenBirthday.getMonths();
+            int days = betweenBirthday.getDays();
+
+            System.out.println("You are " + years + " years, " + months + " months " + days + " days.");
+
+        } catch(DateTimeParseException e) {
+            System.out.println("Wrong date format, try do it again.");
+        }
+
+    }
+}
