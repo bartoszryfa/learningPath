eca5f3978c96f23240aa987e64143ec168b108a3
Bartosz Ryfa <132827271+bartoszryfa@users.noreply.github.com>
2023-11-01 13:14:34 +0100

    edit file

--- Date/StringToDate/Main.java
+++ Date/StringToDate/Main.java
@@ -1,22 +1,33 @@
+package org.example;
+
+import javax.management.MBeanException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.time.Period;
+import java.time.format.DateTimeParseException;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        LocalDate now = LocalDate.now();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        LocalDate dateNow = LocalDate.now();
 
-        // date to String
-        DateTimeFormatter parseDateFormat = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
-        DateTimeFormatter parseDateFormat1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d'th' MMMM");
+        System.out.println("When is your birthday? (####-01-31)");
+        String dateString = reader.readLine();
 
-        String formatDateString = now.format(formatter);
-        System.out.println(formatDateString);
+        try {
+            LocalDate dateOfBirth = LocalDate.parse(dateString);
+            Period betweenBirthday = Period.between(dateOfBirth, dateNow);
+            int years = betweenBirthday.getYears();
+            int months = betweenBirthday.getMonths();
+            int days = betweenBirthday.getDays();
+            LocalDate now = LocalDate.now();
 
-        // String to date
-        String dataString = "10.02.2023";
-        LocalDate parsed = LocalDate.parse(dataString, parseDateFormat1);
-        System.out.println(parsed);
+            System.out.println("You are " + years + " years, " + months + " months " + days + " days.");
+
+            } catch(DateTimeParseException e) {
+            System.out.println("Wrong date format, try do it again.");
+            }
     }
-}
