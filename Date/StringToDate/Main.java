e24b79ecb6d4ae0e15b2eaf2ede2a01b7e963ae8
Bartosz Ryfa <132827271+bartoszryfa@users.noreply.github.com>
2023-11-01 13:05:36 +0100

    add new file

--- Date/StringToDate/Main.java
+++ Date/StringToDate/Main.java
@@ -1,34 +1,22 @@
-package org.example;
-
-import javax.management.MBeanException;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.time.LocalDate;
-import java.time.Period;
-import java.time.format.DateTimeParseException;
-
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        LocalDate dateNow = LocalDate.now();
+        LocalDate now = LocalDate.now();
 
-        System.out.println("When is your birthday? (####-01-31)");
-        String dateString = reader.readLine();
+        // date to String
+        DateTimeFormatter parseDateFormat = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
+        DateTimeFormatter parseDateFormat1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d'th' MMMM");
 
-        try {
-            LocalDate dateOfBirth = LocalDate.parse(dateString);
-            Period betweenBirthday = Period.between(dateOfBirth, dateNow);
-            int years = betweenBirthday.getYears();
-            int months = betweenBirthday.getMonths();
-            int days = betweenBirthday.getDays();
+        String formatDateString = now.format(formatter);
+        System.out.println(formatDateString);
 
-            System.out.println("You are " + years + " years, " + months + " months " + days + " days.");
-
-        } catch(DateTimeParseException e) {
-            System.out.println("Wrong date format, try do it again.");
-        }
-
+        // String to date
+        String dataString = "10.02.2023";
+        LocalDate parsed = LocalDate.parse(dataString, parseDateFormat1);
+        System.out.println(parsed);
     }
 }
